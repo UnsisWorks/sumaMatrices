a9c8dd97d991323fc7c6d0aa145cb89b57ed147e
unsis <jared0192galicia@gmail.com>
2023-03-08 09:25:21 -0600

    Se corrigen erroes

--- src/dev/init.java
+++ src/dev/init.java
@@ -3,45 +3,68 @@ package dev;
 import java.util.Scanner;
 
 public class init {
-
-	static Scanner input = new  Scanner(System.in);
 	
-	public static void main(String[] args) {
+	public static void main(String[] args){
+	
+		int i;
+		int j;
+		int n;
+		int m;
 		
-		int mat1[][] = new int[2][2];
-		int mat2[][] = new int[2][2];
+		Scanner input = new  Scanner(System.in);
 		
-		llenado(mat1 , mat2);
+		System.out.print("Inserte el número de filas: ");
+		n=input.nextInt();
 		
-		for (int i = 0; i < mat1.length; i++){
-			for (int j = 0; j < mat1.length; j++){
-				System.out.print("[ " + mat1[i][j] + " ] ");
-			}
-			System.out.println("");
-						
-		}
+		System.out.print("Inserte el número de columnas: ");
+		m=input.nextInt();
+		
+		int M1 [][]= new int[m][n];
+		int M2 [][]= new int[m][n];
+		int M3 [][]= new int[m][n];
+		
+		llenado(M1);
+		llenado(M2);
+		sumar(M1,M2,M3);
+		System.out.println("");
+		imp(M3);
+		
+		input.close();
 	}
 	
-	public static void llenado (int[][] mat1, int[][] mat2) {
+	public static void llenado (int[][] mat1) {
+		
+		Scanner input = new  Scanner(System.in);
 
-		System.out.println("Lenado de matriz uno");
+		System.out.println("Lenado de matriz");
 		
 		for (int i = 0; i < mat1.length; i++){
-			for (int j = 0; j < mat1.length; j++){
-				System.out.print("Ingrese el valor [ " + i + "] [" + j + " ]: ");
+			for (int j = 0; j < mat1[0].length; j++){
+				System.out.print("Ingrese el valor [ " + (i+1) + "] [" + (j+1) + " ]: ");
 				mat1[i][j] = input.nextInt();
 			}
 			
 		}
-
-		System.out.println("Llenado de matriz dos");
 		
-		for (int i = 0; i < mat2.length; i++){
-			for (int j = 0; j < mat2.length; j++){
-				System.out.print("Ingrese el valor [ " + i + "] [" + j + " ]: ");
-				mat2[i][j] = input.nextInt();
+
+	}
+	
+	public static void sumar (int[][] M1, int[][] M2, int[][] M3 ) {
+		
+		for (int i = 0; i < M2.length; i++) {
+			for (int j = 0; j < M2[0].length; j++) {
+				M3[i][j]= M1[i][j] + M2[i][j];
 			}
-			
 		}
 	}
+	
+	public static void imp (int[][] M3) {
+		for (int i = 0; i < M3.length; i++) {
+			for (int j = 0; j < M3[0].length; j++) {
+				System.out.print(M3[i][j]);
+			}
+			System.out.println("");
+		}
+	}
+	
 }
